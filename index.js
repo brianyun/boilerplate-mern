185891cfc3d8d43c1e5e3f56241faf8fb8f50fc8
윤희상 <huisang.growth@gmail.com>
2020-09-01 14:18:13 +0900

    add config, mongoose, post

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose
-	.connect(
-		"mongodb+srv://hsyun:cristiano7!@boilerplate-mern.dxc3p.mongodb.net/test?retryWrites=true&w=majority",
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-			useFindAndModify: true,
-		}
-	)
+	.connect(config.mongoURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: true,
+	})
 	.then(() => console.log("Mongo DB connected..."))
 	.catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+	const user = new User(req.body);
+	user.save((err, userInfo) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).json({
+			success: true,
+		});
+	});
 });
 
 app.listen(port, () => {
